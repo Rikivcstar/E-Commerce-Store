8add371e07e90e21dc5cfc73c35fc713709d2ec7
Rikivcstar <rikireza3r@gmail.com>
2026-08-01 02:24:36 +0700

    implement moota paynment

--- app/Listeners/GeneratePaymentLinkListener.php
+++ app/Listeners/GeneratePaymentLinkListener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\SalesOrderCreated;
+use App\Events\SalesOrderCreatedEvent;
 use App\Services\PaymentMethodQueryService;
 
 
@@ -19,7 +20,7 @@ class GeneratePaymentLinkListener
     /**
      * Handle the event.
      */
-    public function handle(SalesOrderCreated $event): void
+    public function handle(SalesOrderCreatedEvent $event): void
     {
         app(PaymentMethodQueryService::class)
             ->getDriver(
